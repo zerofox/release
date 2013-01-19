a9f9f2160c75a9cbf3261a59e89b02e71bcdaeb6
Rait Tammik <irxuke@gmail.com>
2013-01-19 22:28:22 +0200

    rewrite comments in move.php

--- move.php
+++ move.php
@@ -1,23 +1,31 @@
 <?php
 
-// 	Skript failide jagamiseks enam-vähem võrdse suurusega 25 kausta
-//	kaustad 1 ... 25 peavad olema eelnevalt loodud
-//	probleeme esineb sulgude ja ülakomadega (failinimedes)
-//	ette nähtud käivitamiseks *nix käsurealt "php move.php".
-//	jagatavate failide asukoht: PWD/$dir
-//	kaustad 1..25 peavad samuti asuma PWD's
+/*
+script to divide files to folders (round-robin)
+original purpose: to divide several thousand mp3's to manageable chunks
 
-//	Released under GNU GPL by creator Rait Tammik (irxuke@gmail.com) on 02.02.2012
+CONFIGURATION:
+$source_dir - files are taken from there
+$dir_count - number of directories to divide between (directories must be created in advance)
 
-$dir = 'jagada';
-$files = scandir ($dir);
+some filenames (brackets and other special marks) won't be parsed correctly and will not be moved
+(will be printed out as errors)
+
+written to be run under *nix CLI
+
+Released under GNU GPL by creator Rait Tammik (irxuke@gmail.com) on 02.02.2012
+*/
+
+$source_dir = 'jagada';
+$dir_count = 25;
+$files = scandir ($source_dir);
 $i = 1;
 
 foreach  ($files as $file) {
-	$cmd = 'mv \'' . $dir . '/' . $file . '\' ' . $i . '/';
+	$cmd = 'mv \'' . $source_dir . '/' . $file . '\' ' . $i . '/';
 	echo $cmd . PHP_EOL;
 	exec ($cmd);
-	if ($i < 25)  $i++;
+	if ($i < $dir_count)  $i++;
 	else $i = 1;
 }
 
